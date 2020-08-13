14ea045b4f52a37e88a0a12113c6cf10d6d42493
werifesteria <64672399+werifesteria@users.noreply.github.com>
2020-08-14 01:32:21 +0800

    Update diy-part1.sh
    #添加rufengsuixing的AdGuardHome插件
    #添加lwz322的K3屏幕插件
    #替换lwz322的K3屏幕驱动
    #替换K3的无线驱动

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,20 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#添加rufengsuixing的AdGuardHome插件
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/rufengsuixing
+
+#添加lwz322的K3屏幕插件
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+
+#替换lwz322的K3屏幕驱动
+rm -rf package/lean/k3screenctrl/
+git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screenctrl/
+
+#替换K3的无线驱动
+wget https://github.com/Hill-98/phicommk3-firmware/archive/master.zip -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip
+unzip package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip -d package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/
+rm -rf package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip
+mv package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/phicommk3-firmware-master/brcmfmac4366c-pcie.bin.69027 package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+rm -rf package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/phicommk3-firmware-master
