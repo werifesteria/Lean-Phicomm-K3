038b402da7e70cb3d30e0675e47ebfe0e3108f1f
werifesteria <64672399+werifesteria@users.noreply.github.com>
2020-08-15 23:18:28 +0800

    1.3

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,10 +23,6 @@ sed -i 's/2.openwrt.pool.ntp.org/2.ntp3.aliyun.com/g' package/base-files/files/b
 sed -i 's/3.openwrt.pool.ntp.org/3.ntp4.aliyun.com/g' package/base-files/files/bin/config_generate
 echo '修改NTP设置 OK!'
 
-#主页添加CPU温度
-#sed -i '725a \ \t\t<tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec("sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-4")%></td></tr>' package/lean/autocore/files/index.htm
-#echo 'Add CPU Temperature in Index OK!'
-
 #修改内核版本为5.4
 sed -i 's/4.19/5.4/g' target/linux/bcm53xx/Makefile
 cat target/linux/bcm53xx/Makefile |grep KERNEL_PATCHVER
