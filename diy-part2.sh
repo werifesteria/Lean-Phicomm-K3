47b92b0615b0de4d0c90615d1ccef559a487952c
werifesteria <64672399+werifesteria@users.noreply.github.com>
2020-08-16 09:04:41 +0800

    2.0

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,20 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+#修改NTP设置
+sed -i 's/0.openwrt.pool.ntp.org/0.ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/1.ntp2.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/2.ntp3.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/3.ntp4.aliyun.com/g' package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep ntp1.aliyun.com
+echo 'Alert NTP Settings OK!====================='
+
+#修改主机名
+sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-Phicomm-K3'/g" package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo 'Alert Hostname OK!====================='
+
 #添加主页的CPU温度显示
 sed -i "/<tr><td width=\"33%\"><%:Load Average%>/a \ \t\t<tr><td width=\"33%\"><%:CPU Temperature%></td><td><%=luci.sys.exec(\"sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-4\")%></td></tr>" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-echo "Add CPU Temperature in Admin Index OK"
+cat feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm |grep Temperature
+echo "Add CPU Temperature in Admin Index OK====================="
