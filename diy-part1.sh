6a0bf48dd52e1cf14cd6ac1a292565785c07d85a
werifesteria <64672399+werifesteria@users.noreply.github.com>
2020-08-17 18:02:34 +0800

    3.0

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,39 +13,37 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#添加lienol feed源
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cat feeds.conf.default |grep lienol
+echo '====================Add lienol feed source OK!===================='
 
 #修改内核版本为5.4
 sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=5.4/g' target/linux/bcm53xx/Makefile
 cat target/linux/bcm53xx/Makefile |grep KERNEL_PATCHVER
-echo 'Alert Kernel Version 5.4 OK!===================='
+echo '====================Alert Kernel Patchver to 5.4 OK!===================='
 
 #添加rufengsuixing的AdGuardHome插件
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/rufengsuixing
-sed -i '1,$d' package/rufengsuixing/root/usr/share/AdGuardHome/links.txt
-echo 'https://static.adguard.com/adguardhome/release/AdGuardHome_linux_armv5.tar.gz'>>package/rufengsuixing/root/usr/share/AdGuardHome/links.txt
-cat package/rufengsuixing/root/usr/share/AdGuardHome/links.txt
-echo 'Add AdGuardHome plug OK!===================='
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+sed -i '1,$d' package/lean/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
+echo 'https://static.adguard.com/adguardhome/release/AdGuardHome_linux_armv5.tar.gz'>>package/lean/luci-app-adguardhome/root/usr/share/AdGuardHome/li>cat package/lean/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
+echo '====================Add AdGuardHome Plug OK!===================='
 
 #添加likanchen的K3屏幕插件
 git clone https://github.com/likanchen/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
 ls -la package/lean/ |grep luci-app-k3screenctrl
-echo 'Add k3screen Plug OK!===================='
+echo '====================Add k3screen Plug OK!===================='
 
 #替换likanchen的K3屏幕驱动
 rm -rf package/lean/k3screenctrl
 git clone https://github.com/likanchen/k3screenctrl_build.git package/lean/k3screenctrl/
 sed -i 's/@TARGET_bcm53xx_DEVICE_phicomm-k3 +@KERNEL_DEVMEM +//g' package/lean/k3screenctrl/Makefile
 cat package/lean/k3screenctrl/Makefile |grep DEPENDS
-echo 'Add k3screen Drive OK!'
+echo '====================Add k3screen Drive OK!===================='
 
 #替换K3的无线驱动
-wget https://github.com/Hill-98/phicommk3-firmware/archive/master.zip -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip
-unzip package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip -d package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/
-mv package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/phicommk3-firmware-master/brcmfmac4366c-pcie.bin.69027 package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
-echo 'Instead of k3-firmware OK!'
-rm -rf package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip
-echo 'Delete wget k3-firmware zip file OK!'
+wget -nv https://github.com/Hill-98/phicommk3-firmware/archive/master.zip -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zipunzip package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip -d package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/
+mv package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/phicommk3-firmware-master/brcmfmac4366c-pcie.bin.69027 package/lean/k3-brcmfmac4366>rm -rf package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip
 rm -rf package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/phicommk3-firmware-master
-echo 'Delete temp or release files!'
+ls -la package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/ |grep 4366c
+echo '====================Delete temp or release files!===================='
