5745c8de740945f9ef00063546c30336b4393cca
werifesteria <64672399+werifesteria@users.noreply.github.com>
2020-08-27 01:25:34 +0800

    3.1
    ban alert ntp server

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,15 +14,15 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #修改NTP设置
-sed -i "s/'0.openwrt.pool.ntp.org'/'ntp1.aliyun.com'/g" package/base-files/files/bin/config_generate
-sed -i "s/'1.openwrt.pool.ntp.org'/'ntp2.aliyun.com'/g" package/base-files/files/bin/config_generate
-sed -i "s/'2.openwrt.pool.ntp.org'/'ntp3.aliyun.com'/g" package/base-files/files/bin/config_generate
-sed -i "s/'3.openwrt.pool.ntp.org'/'ntp4.aliyun.com'/g" package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep system.ntp.server=
-echo 'Alert NTP Settings OK!====================='
+#sed -i "s/'0.openwrt.pool.ntp.org'/'ntp1.aliyun.com'/g" package/base-files/files/bin/config_generate
+#sed -i "s/'1.openwrt.pool.ntp.org'/'ntp2.aliyun.com'/g" package/base-files/files/bin/config_generate
+#sed -i "s/'2.openwrt.pool.ntp.org'/'ntp3.aliyun.com'/g" package/base-files/files/bin/config_generate
+#sed -i "s/'3.openwrt.pool.ntp.org'/'ntp4.aliyun.com'/g" package/base-files/files/bin/config_generate
+#cat package/base-files/files/bin/config_generate |grep system.ntp.server=
+#echo 'Alert NTP Settings OK!====================='
 
 #修改主机名
-sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-Phicomm-K3'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
 cat package/base-files/files/bin/config_generate |grep hostname=
 echo 'Alert Hostname OK!====================='
 
