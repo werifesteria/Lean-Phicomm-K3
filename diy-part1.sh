baab0e4648234eda43c53474c558b00896d6d901
werifesteria <64672399+werifesteria@users.noreply.github.com>
2020-12-03 10:43:20 +0800

    5.0
    rebuild autoshell

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-#添加lienol feed源
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-cat feeds.conf.default |grep lienol
+#添加xiaorouji的passwall软件源
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+cat feeds.conf.default |grep passwall
 echo '====================Add lienol feed source OK!===================='
 
-#添加jerryk feed源
-#sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-#cat feeds.conf.default |grep jerryk
-#echo '====================Add jerryk feed source OK!===================='
-
-#修改内核版本为5.4
+#修改linux内核为5.4分支
 sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=5.4/g' target/linux/bcm53xx/Makefile
 cat target/linux/bcm53xx/Makefile |grep KERNEL_PATCHVER
 echo '====================Alert Kernel Patchver to 5.4 OK!===================='
@@ -34,18 +29,21 @@ git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screen
 cat package/lean/k3screenctrl/Makefile |grep DEPENDS
 echo '====================Add k3screen Drive OK!===================='
 
-#替换K3的无线驱动
-wget -nv https://github.com/Hill-98/phicommk3-firmware/archive/master.zip -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip
-unzip package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip -d package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/
-mv package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/phicommk3-firmware-master/brcmfmac4366c-pcie.bin.69027 package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
-rm -rf package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/master.zip
-rm -rf package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/phicommk3-firmware-master
-ls -la package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/ |grep 4366c
-echo '====================Delete temp or release files!===================='
-
-#移除bcm53xx的其他机型
+#移除bcm53xx中的其他机型
 sed -i '141,385d' target/linux/bcm53xx/image/Makefile
 sed -i '150,179d' target/linux/bcm53xx/image/Makefile
 sed -i 's/k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
 cat target/linux/bcm53xx/image/Makefile |grep DEVICE_PACKAGES
 echo '====================Remove other devices of bcm53xx!===================='
+
+#替换K3的无线驱动为ac86u
+wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.ac88u
+mv brcmfmac4366c-pcie.bin.ac88u package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+chmod 0644 package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+echo '====================Replace k3wireless firmware OK!===================='
+
+#替换K3的无线驱动为69027
+#wget -nv https://github.com/Hill-98/phicommk3-firmware/raw/master/brcmfmac4366c-pcie.bin.69027
+#mv brcmfmac4366c-pcie.bin.69027 package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+#chmod 0644 package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+#echo '====================Replace k3wireless firmware OK!===================='
